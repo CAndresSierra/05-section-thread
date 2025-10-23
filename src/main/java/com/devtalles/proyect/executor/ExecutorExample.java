2c3516e71d2abc44a5c6fb8e4a621d8b14c115c9
CAndresSierra <camilofullstack@gmail.com>
2025-10-23 11:55:57 -0500

    feat(executor): Add executor examples
    Add examples for Fixed, Cached and Single thread pools.
    This helps to understand executor service usages.

--- src/main/java/com/devtalles/proyect/executor/ExecutorExample.java
+++ src/main/java/com/devtalles/proyect/executor/ExecutorExample.java
@@ -6,11 +6,40 @@ import java.util.concurrent.Executors;
 
 public class ExecutorExample {
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(2);
-        executor.execute(() -> System.out.println("Task A " + Thread.currentThread().getName()));
-        executor.execute(() -> System.out.println("Task B " + Thread.currentThread().getName()));
-        executor.execute(() -> System.out.println("Task C " + Thread.currentThread().getName()));
-        executor.shutdown();
+
+        Runnable task = () -> {
+            System.out.println("Eexecuting task " + Thread.currentThread().getName());
+            try{
+               Thread.sleep(1500);
+            } catch (InterruptedException e){
+                System.out.println(e.getMessage());
+            }
+            System.out.println("task completed in thread " + Thread.currentThread().getName());
+        };
+
+        System.out.println("Executing newFixedThreadPool");
+        ExecutorService fixedPool = Executors.newFixedThreadPool(2);
+
+        for(int i = 0; i <= 5; i++){
+            fixedPool.execute(task);
+        }
+        fixedPool.shutdown();
+
+        System.out.println("Executing newCachedThreadPool");
+        ExecutorService cachedPool = Executors.newCachedThreadPool();
+
+        for(int i = 0; i <= 5; i++){
+            cachedPool.execute(task);
+        }
+        cachedPool.shutdown();
+
+        System.out.println("Executing newCachedThreadPool");
+        ExecutorService single = Executors.newSingleThreadExecutor();
+
+        for(int i = 0; i <= 5; i++){
+            single.execute(task);
+        }
+        single.shutdown();
 
     }
 }
